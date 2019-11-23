e1710ca15638ba21c94155646816eb41a4644335
www <l2u0k0a0s.ld@gmail.com>
2019-11-23 16:14:08 +0100

    Create volume.js

---
+++ volume.js
@@ -0,0 +1,20 @@
+var Video = document.getElementById('Video');
+var Button = document.getElementById('volume-btn');
+Button.addEventListener('click', function() {
+    if (Video.muted === true) {
+        Video.muted = false;
+        Video.volume = 0.05;
+        document.getElementById("volume-btn").src = "assets/media/vol-on.png";
+    }
+    else{
+        if (Video.volume === 0){
+            Video.volume = 0.05;
+            document.getElementById("volume-btn").src = "assets/media/vol-on.png";
+        }
+        else{
+            Video.volume = 0;
+            document.getElementById("volume-btn").src = "assets/media/vol-off.png";
+        }
+        
+    }
+    });
